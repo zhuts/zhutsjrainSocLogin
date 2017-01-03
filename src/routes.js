0fba252619ccced3f8642c89a7b1b9a7166065cc
zhuts <zhutsell@gmail.com>
2017-01-02 17:35:09 -0800

    mostly finished

--- src/routes.js
+++ src/routes.js
@@ -1,3 +1,15 @@
 import React from 'react';
 import { Route, IndexRoute} from 'react-router';
 import Layout from './components/Layout';
+import IndexPage from './components/IndexPage';
+import NotFoundPage from './components/404notfound';
+import LoggedInPage from './components/LoggedInPage';
+
+const routes = (
+  <Route path="/" component={Layout}>
+    <Route path="loggedIn" component={LoggedInPage}/>
+    <Route path="*" component={NotFoundPage}/>
+  </Route>
+);
+
+export default routes;
